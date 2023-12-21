5e9302d3a8a84d0af2011e74cd0ff1e5e59a6174
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 19:11:21 +0100

    small typeof changes

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,14 +1,15 @@
 // Exercise 1
-const firstData = prompt("Enter first data: ");
-const secondData = prompt("Enter second data: ");
-const thirdData = prompt("Enter third data: ");
-alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
+// const firstData = prompt("Enter first data: ");
+// const secondData = prompt("Enter second data: ");
+// const thirdData = prompt("Enter third data: ");
+// alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
 
 // Exercise 2
 const number = prompt("Enter your number: ");
-if (number.length === 5 && typeof number === "number") {
+if (number.length == 5 && typeof parseInt(number) === "number") {
   const result = number.split("").join(" ");
   alert(result);
 } else {
+  console.log(number.length);
   alert("Write five-digit number!");
 }
