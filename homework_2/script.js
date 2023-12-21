ebedf237802ab83921bd5fadbb8a1d414254ca43
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 18:46:05 +0100

    Update script.js

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,8 +1,10 @@
+// Exercise 1
 const firstData = prompt("Enter first data: ");
 const secondData = prompt("Enter second data: ");
 const thirdData = prompt("Enter third data: ");
 alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
 
+// Exercise 2
 const number = prompt("Enter your number: ");
 if (number.length === 5 && typeof number === "number") {
   const result = number.split("").join(" ");
