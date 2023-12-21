8007ddfca92ce14951484047af99f0fb04aa8a61
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 18:34:06 +0100

    homework2 completed

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,1 +1,12 @@
+// const firstData = prompt("Enter first data: ");
+// const secondData = prompt("Enter second data: ");
+// const thirdData = prompt("Enter third data: ");
+// alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
 
+const number = prompt("Enter your number: ");
+if (number.length === 5 && typeof number === "number") {
+  const result = number.split("").join(" ");
+  alert(result);
+} else {
+  alert("Write five-digit number!");
+}
