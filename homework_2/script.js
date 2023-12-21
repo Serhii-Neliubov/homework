02b1550dca63e001c3aa1983ac335e2bcc90f86b
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 20:59:24 +0100

    changes with output

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,12 +1,12 @@
 // Exercise 1
-const firstData = prompt("Enter first data: ");
-const secondData = prompt("Enter second data: ");
-const thirdData = prompt("Enter third data: ");
-alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
+// const firstData = prompt("Enter first data: ");
+// const secondData = prompt("Enter second data: ");
+// const thirdData = prompt("Enter third data: ");
+// alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
 
 // Exercise 2
 const number = prompt("Enter your number: ");
-if (number.length == 5 && typeof Number(number) === "number") {
+if (number.length == 5 && typeof number === "number") {
   const result = number.split("").join(" ");
   alert(result);
 } else {
