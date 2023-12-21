41d68339051777b1acec2f67c25f92b2b78586a2
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 19:11:52 +0100

    small changes

--- homework_2/script.js
+++ homework_2/script.js
@@ -1,8 +1,8 @@
 // Exercise 1
-// const firstData = prompt("Enter first data: ");
-// const secondData = prompt("Enter second data: ");
-// const thirdData = prompt("Enter third data: ");
-// alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
+const firstData = prompt("Enter first data: ");
+const secondData = prompt("Enter second data: ");
+const thirdData = prompt("Enter third data: ");
+alert(`Your full data is: ${firstData} ${secondData} ${thirdData}`);
 
 // Exercise 2
 const number = prompt("Enter your number: ");
@@ -10,6 +10,5 @@ if (number.length == 5 && typeof parseInt(number) === "number") {
   const result = number.split("").join(" ");
   alert(result);
 } else {
-  console.log(number.length);
   alert("Write five-digit number!");
 }
