75a1669f2785073b5a28e0dca9598013fae35ae5
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-21 21:57:07 +0100

    fully functional

--- homework_2/script.js
+++ homework_2/script.js
@@ -7,8 +7,20 @@
 // Exercise 2
 const number = prompt("Enter your number: ");
 if (Math.round(number) > 0 && number.length == 5) {
-  const result = number.split("").join(" ");
-  alert(result);
+  const numberSplit = number.split("");
+  let result = [];
+
+  numberSplit.map((number) => {
+    if (parseInt(number)) {
+      result = [...result, number];
+    }
+  });
+
+  if (result.length === 5) {
+    alert(result.slice("").join(" "));
+  } else {
+    alert("Write correct number!");
+  }
 } else {
   alert("Write a positive five-digit number!");
 }
