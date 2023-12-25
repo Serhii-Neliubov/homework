cf63ead0f8042a4dc5712856b3e26157db9e8680
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-25 21:41:59 +0100

    Update script.js

--- homework_4/script.js
+++ homework_4/script.js
@@ -1,6 +1,6 @@
 
 const number1 = prompt('Write the first number: ');
-const number2 = prompt('Write the first number: ');
+const number2 = prompt('Write the second number: ');
 function calculateNumber(number1, number2) {
     alert(`
     ${number1} + ${number2} = ${(number1 + number2).toFixed(2)}
