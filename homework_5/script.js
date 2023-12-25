6566272fb71617c836e50023648dc303f542ce9e
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-25 21:34:49 +0100

    homework 6

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,14 +1,8 @@
 
 const hours = prompt('Write the hours: ');
-function calculateNumber(hours) {
-    if(hours === 0){
-        alert('You must to write anything!')
-    } else {
-        const result = hours*3600
-        alert(`
-        Seconds in ${hours} hours: ${result}
-        `);
-    }
+function calculateSeconds(hours) {
+    const result = hours*3600
+    alert(`Seconds in ${hours} hours: ${result}`);
 }
 
-calculateNumber(Number(hours));
+calculateSeconds(Number(hours));
---
+++ homework_6/script.js
@@ -0,0 +1,10 @@
+
+const number1 = prompt('Write the first number: ');
+const number2 = prompt('Write the second number: ');
+const number3 = prompt('Write the third number: ');
+function averageNumber(number1, number2, number3) {
+    const result = Number(((number1 + number2 + number3) / 3).toFixed(2))
+    alert(`Average result: ${result}`)
+}
+
+averageNumber(number1, number2, number3);
