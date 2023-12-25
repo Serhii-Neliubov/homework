d2a31699cc7fd6410bfb55859f9f0d78b6d807c8
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-25 21:44:54 +0100

    Update script.js

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,7 +1,8 @@
 
 const hours = prompt('Write the hours: ');
 function calculateSeconds(hours) {
-    const result = hours*3600
+    const SECONDS_IN_HOUR = 3600
+    const result = hours*SECONDS_IN_HOUR
     alert(`Seconds in ${hours} hours: ${result}`);
 }
 
