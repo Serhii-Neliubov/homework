9d2b13f9d67959e2e223bafccc3ffc28957a1e99
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-25 21:45:57 +0100

    homework 6

--- homework_6/script.js
+++ homework_6/script.js
@@ -3,7 +3,7 @@ const number1 = prompt('Write the first number: ');
 const number2 = prompt('Write the second number: ');
 const number3 = prompt('Write the third number: ');
 function averageNumber(number1, number2, number3) {
-    const result = Number(((number1 + number2 + number3) / 3).toFixed(2))
+    const result = ((Number(number1) + Number(number2) + Number(number3)) / 3).toFixed(2)
     alert(`Average result: ${result}`)
 
 }
