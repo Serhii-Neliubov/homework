3b78d199920307bbe5a70aadce407508209a27a9
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-28 21:45:43 +0100

    homework 8

--- homework_8/script.js
+++ homework_8/script.js
@@ -2,14 +2,44 @@ const userBirthdayYear = prompt('Write your birthday year: ');
 const userCity = prompt('Write your city: ');
 const userSport = prompt('Write your favorite sport: ');
 
-const currentYear = new Date().getFullYear()
+const currentYear = 2023;
 
+let forCityMessage = '';
+let forSportMessage = '';
+let forBirthdayMessage = '';
 
-switch (true){
-    case userCity === 'Kharkov':
-        break;
-}
+    if(userCity === 'Київ'){
+        forCityMessage = 'Ти живеш у столиці України';
+    } else if(userCity === 'Лондон'){
+        forCityMessage = 'Ти живеш у столиці Англії';
+    } else if(userCity === 'Вашингтон'){
+        forCityMessage = 'Ти живеш у столиці Америки';
+    } else if(userCity === null) {
+        forCityMessage = 'Шкода, що ти не захотів ввести своє місто';
+    } else {
+        forCityMessage = `Ти живеш у місті ${userCity}`
+    }
 
-alert(`
-    Your age is: ${currentYear - userBirthdayYear}
-`)
+    if(userSport === 'Бокс'){
+        forSportMessage = 'Ти хочеш бути як Віталій Кличко?';
+    } else if(userSport === 'Футбол'){
+        forSportMessage = 'Ти хочеш бути як Мессі?';
+    } else if(userSport === 'Теніс'){
+        forSportMessage = 'Ти хочеш бути як Тімобол?';
+    } else if(userSport === null) {
+        forSportMessage = 'Шкода, що ти не захотів ввести свій спорт';
+    }else {
+        forSportMessage = `Ти займаєшся спортом під назвою: ${userSport}`
+    }
+
+    if(userBirthdayYear === null){
+        forBirthdayMessage = 'Шкода, що ти не захотів ввести свій рік народження';
+    } else if(userBirthdayYear){
+        forBirthdayMessage = `Your age is: ${currentYear - userBirthdayYear}`;
+    }
+
+    alert(`
+        ${forBirthdayMessage},
+        ${forCityMessage},
+        ${forSportMessage},
+    `);
