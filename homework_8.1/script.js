941cd5a72a178c543ed307c589bd5e82c271a11f
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-28 22:56:27 +0100

    homework 8.1

---
+++ homework_8.1/script.js
@@ -0,0 +1,40 @@
+const cities = {'Київ': 'України', 'Лондон': 'Англії', 'Вашингтон': ' Америки',}
+const sports = {'Бокс': 'Кличко', 'Футбол': 'Мессі', 'Теніс': ' Тімобол',}
+
+const userBirthdayYear = prompt('Write your birthday year: ');
+const userCity = prompt('Write your city: ');
+const userSport = prompt('Write your favorite sport: ');
+
+const currentYear = new Date().getFullYear();
+
+let forCityMessage = '';
+let forSportMessage = '';
+let forBirthdayMessage = '';
+
+    if(Object.keys(sports).find(() => userSport)){
+        forSportMessage = `Ти хочеш бути як ${sports[userSport]}?`;
+    } else if(userSport) {
+        forSportMessage = `Ти займаєшся спортом під назвою: ${userSport}`
+    } else {
+        forSportMessage = 'Прикро, що ти нічого не написав у спорт :('
+    }
+
+    if(Object.keys(cities).find(() => userCity)){
+        forCityMessage = `Ти хочеш бути як ${cities[userCity]}?`;
+    } else if(userCity) {
+        forCityMessage = `Ти займаєшся спортом під назвою: ${userCity}`
+    } else {
+        forCityMessage = 'Прикро, що ти нічого не написав у спорт :('
+    }
+
+    if(userBirthdayYear){
+        forBirthdayMessage = `You are: ${currentYear - userBirthdayYear}`;
+    } else {
+        forCityMessage = 'Прикро, що ти нічого не написав у рік народження :('
+    }
+
+    alert(`
+        ${forBirthdayMessage},
+        ${forCityMessage},
+        ${forSportMessage}
+    `);
