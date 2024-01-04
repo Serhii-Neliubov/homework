93a6a696804ee52654b9855955ef3c78eeb8c8b9
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2024-01-04 22:59:48 +0100

    homework 11 updated

--- homework_11/script.js
+++ homework_11/script.js
@@ -4,44 +4,42 @@ const halfNumbersResult = [];
 for (let i = 20; i <= 30; i += 0.5){
     halfNumbersResult.push(i);
 }
-console.log(halfNumbersResult);
+console.log(halfNumbersResult.join(' '));
 
 // Exercise 2
 console.log('Exercise 2');
-const dollarsPriceResult = [];
-for (let i = 20; i <= 100; i += 10){
-    dollarsPriceResult.push(i * 27);
+for (let i = 10; i <= 100; i += 10){
+    console.log(`${i} * 27 = `, i * 27)
 }
-console.log(dollarsPriceResult);
 
 // Exercise 3
-// console.log('Exercise 3');
-// const number = prompt('Write the number: ')
-//
-// if(!Math.round(Number(number))){
-//     alert('Write the correct number')
-// }
-//
-// for (let i = 1; i*i < number; i++){
-//     if(i <= 100){
-//         console.log(`${i} * ${i} (${i*i}) < `, number);
-//     }
-// }
+console.log('Exercise 3');
+const number = prompt('Write the number: ')
+
+if(!Math.round(Number(number))){
+    alert('Write the correct number')
+}
+
+for (let i = 1; i*i < number; i++){
+    if(i <= 100 && Math.pow(100, 2) >= number){
+        console.log(`${i} * ${i} (${i*i}) < `, number);
+    }
+}
 // Exercise 4
-// console.log('Exercise 4');
-// const isSimpleNumber = prompt('Write the number: ')
-// let isSimpleNumberResult = 0;
-//
-// if(!Math.round(Number(isSimpleNumber)) || Number(isSimpleNumber) < 2){
-//     alert('Write the correct number')
-// }
-//
-// for (let i = 1; i <= isSimpleNumber; i++){
-//     if(isSimpleNumber % i === 0){
-//         isSimpleNumberResult++;
-//     }
-// }
-// isSimpleNumberResult === 2 ? console.log('Simple number!') : console.log('Not a simple number!')
+console.log('Exercise 4');
+const simpleNumberCount = prompt('Write the number: ')
+let simpleNumberResult = 0;
+
+if(simpleNumberCount % 1 || Number(simpleNumberCount) < 2){
+    alert('Write the correct number')
+}
+
+for (let i = 1; i <= simpleNumberCount; i++){
+    if(simpleNumberCount % i === 0){
+        simpleNumberResult++;
+    }
+}
+console.log(simpleNumberResult === 2 ? 'Simple number!' : 'Not a simple number!');
 // Exercise 5
 console.log('Exercise 5');
 const userNumber = prompt('Write the number: ')
@@ -52,8 +50,10 @@ if(!Math.round(Number(userNumber))){
 }
 
 for (let i = 1; i <= userNumber; i++){
-    if(Math.pow(i, 3) === Number(userNumber)) {
+    if(Math.pow(3, i) === Number(userNumber)) {
         dividerNumber = i;
-        alert(`Yes, this is ${dividerNumber}^3!`)
+        alert(`Yes, this is 3^${dividerNumber}!`)
     }
 }
+
+!dividerNumber && alert('There is no such number');
