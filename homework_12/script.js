053df8506889c7838d07bbb2d0c5414883f61f9f
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2024-01-15 21:58:14 +0100

    updated homework 12

--- homework_12/script.js
+++ homework_12/script.js
@@ -1,23 +1,28 @@
-const length = prompt("Write array length:");
+const length = parseInt(prompt("Write array length:"));
 const array = [];
 
-if(isNaN(parseInt(length))){
+if (isNaN(length)) {
     alert('Write the correct number: ');
 }
 
-function compareNumbers(a, b) {
-    return a - b;
-}
-
-for (let i = 0; i < parseInt(length); i++) {
-    const element = prompt("Write the array element:");
-    if(Number(element) >= 0){
-        array.push(Number(element));
+function compareElements(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a - b;
     } else {
-        array.push(element)
+        return String(a).localeCompare(String(b));
     }
 }
 
-array.sort(compareNumbers).splice(1, 3);
+for (let i = 0; i < length; i++) {
+    const element = prompt("Write the array element:");
+    if (Number(element) >= 0 || element === '0') {
+        array.push(Number(element));
+    } else {
+        array.push(element);
+    }
+}
+
+array.sort(compareElements);
+array.splice(1, 3);
 
 console.log("Result:", array);
