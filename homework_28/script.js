b84688f3e00e1270a0de709f9dfa7b3a57bf163e
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2024-03-01 17:53:54 +0100

    completed homework 28

---
+++ homework_28/script.js
@@ -0,0 +1,20 @@
+const images = [
+    'img1.jpg',
+    'img2.jpg',
+    'img3.jpeg',
+    'img4.jpg',
+    'img5.jpg',
+    'img6.jpg',
+    'img7.jpg',
+    'img8.jpg',
+    'img9.jpg',
+];
+
+const index = Math.floor(Math.random() * images.length);
+const image = images[index];
+
+const imageBlock = document.createElement('img');
+
+imageBlock.src = './images/' + image;
+
+document.body.appendChild(imageBlock);
